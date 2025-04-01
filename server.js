531ed55e92d67251f74b7d22e6fa1ef738a63389
Karthikcahry23 <lingojikarthikchary@gmail.com>
2025-04-01 18:34:02 +0530

    bcrypt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
